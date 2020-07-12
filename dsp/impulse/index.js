6a813ab5d8a4fb64ce377d652b6ccc9602524a24
rabidmachine9 <konpatsourakis@gmail.com>
2020-07-12 16:47:15 +0300

    added a basic impulse module with a dust feature

---
+++ dsp/impulse/index.js
@@ -0,0 +1,25 @@
+export const impulse = async(context) => {
+  var pulse = 0;
+  return ({n}) => {
+    pulse = (n === 0 ? 1 : 0);
+    return pulse;
+  }
+}
+
+
+export const dust = async(context, density) => {
+  var pulse;
+  var coin;
+  var dens = density ;
+  var probability;
+
+  return () => {
+    probability = Math.random();
+    if(probability < dens){
+      pulse = Math.random();
+      return pulse;
+    }else {
+      return 0;
+    }
+  }
+}
