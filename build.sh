4d5df5c9b56b77a642498e98bd62b9fdc17d4e65
George Stagas <gstagas@gmail.com>
2020-08-19 15:30:09 +0400

    add build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+# merge remote changes
+git submodule update --remote --merge --recursive
+
+# TODO: build
+
+# update backend
+cd backend
+git add .
+git commit -m "update build"
+cd ..
+
+# push local changes
+git submodule foreach --recursive git push origin master
